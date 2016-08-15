57bcabb1069f92db5bb755e3c01145e4b18b54fd
wu <wu@fishlab.org>
2016-08-15 12:04:21 +0800

    do not write source map file when development

--- gulptasks/static-bundle.js
+++ gulptasks/static-bundle.js
@@ -5,12 +5,17 @@ module.exports = function (gulp, isRelease) {
     var concat = require('gulp-concat');
 
     gulp.task('bundle-static', function () {
-        var chain = gulp.src(files).pipe(sourcemaps.init());
-
+        var chain = gulp.src(files)
+            .pipe(concat('static.js'))
         // Compress if building for release
-        if (isRelease)
-             chain = chain.pipe(uglify({mangle: { keep_fnames: true}, compress: {keep_fnames: true} }));
-
-        return chain.pipe(concat('static.js')).pipe(sourcemaps.write()).pipe(gulp.dest('./www/build'));
+        if (isRelease){
+            
+             chain = chain
+                .pipe(sourcemaps.init())
+                .pipe(uglify({mangle: { keep_fnames: true}, compress: {keep_fnames: true} }))
+                .pipe(sourcemaps.write('./'))
+                ;
+        }
+        return chain.pipe(gulp.dest('./www/build'));
     });
 };
