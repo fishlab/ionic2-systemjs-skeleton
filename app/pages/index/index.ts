1486bcaa70e06abae0cbeb018a87e8ca1b9cf753
wu <wu@fishlab.org>
2016-08-22 10:40:53 +0800

    remove virtual scroll

--- app/pages/index/index.ts
+++ app/pages/index/index.ts
@@ -27,18 +27,32 @@ export class IndexPage {
     })
   }
   //mock
-  loadProductsMock(){
-    return new  Promise ((resolve,reject) =>{
-      for(var i =0;i<10;i++){
-        this.products.push({
-          id: this.page*10+i,
-          name:"product" + this.page*10+i,
-          unit_price:2.15
-        });
-      }
-      resolve();
+  loadProductsMock() {
+    return new Promise((resolve, reject) => {
+
+      setTimeout(() => {
+        // this.products =[];
+        for (var i = 0; i < 10; i++) {
+          this.products.push({
+            id: this.page * 10 + i,
+            name: "product" + (this.page * 10 + i),
+            unit_price: 2.15
+          });
+        }
+        resolve();
+
+
+      }, 1500);
+
     })
   }
+
+  // myHeaderFn(record, recordIndex, records) {
+  //   if (recordIndex % 20 === 0) {
+  //     return 'Header ' + recordIndex;
+  //   }
+  //   return null;
+  // }
 
   openDetail(product) {
     this.navCtrl.push(DetailPage);
