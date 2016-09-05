a8d9ce282bbc891339284c67244b79a5a54cbe3a
wei wu <wei wu>
2016-09-05 22:46:30 +0800

    shop and categories 0

--- app/pages/about/about.ts
+++ app/pages/about/about.ts
@@ -1,10 +1,25 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
-
+import {Http} from "../../services/http";
+import {api} from "../../services/config";
 @Component({
   templateUrl: 'build/pages/about/about.html'
 })
 export class AboutPage {
-  constructor(private navCtrl: NavController) {
+  private categories:any;
+  private shop:any;
+  constructor(private navCtrl: NavController,
+  private http: Http
+  ) {
+    this.loadShopAndCategories();
+  }
+
+  loadShopAndCategories(){
+    this.http.get( api('/user/shop/shop-and-categories'))
+    .toPromise()
+    .then( ret =>{
+      this.shop = ret.shop;
+      this.categories = ret.categories;
+    });
   }
 }
